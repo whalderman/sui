136df13ddf3056bea591fc255004881cdad2c90c
Warren Halderman <halderman.warren@gmail.com>
2025-07-24 16:26:13 +0900

    Retry workspaces, still broken
    This reverts commit 1bebc8a586051594f591125f01b440e38c6fdcee.

--- src/throttle/mod.ts
+++ src/throttle/mod.ts
@@ -3,7 +3,7 @@
  * Provides utilities for throttling asynchronous operations.
  */
 
-import { intBetween } from "../random/integer.ts";
+import { intBetween } from "@wjh/random/integer";
 
 /**
  * Creates an async generator that yields throttle intervals and allows dynamic adjustment of throttling strategy.
@@ -38,93 +38,93 @@ import { intBetween } from "../random/integer.ts";
  * ```
  */
 export async function* create(
-  {
-    defaultThrottleMillis,
-    backoffMultiplier = 2,
-    maxThrottleMillis,
-  }: CreateThrottleOptions,
+	{
+		defaultThrottleMillis,
+		backoffMultiplier = 2,
+		maxThrottleMillis,
+	}: CreateThrottleOptions,
 ): AsyncGenerator<
-  { latestThrottleMillis: number } | undefined,
-  void,
-  | { strategy: "backoff" | "speedup" }
-  | { strategy: "override"; overrideMillis: number }
-  | undefined
+	{ latestThrottleMillis: number } | undefined,
+	void,
+	| { strategy: "backoff" | "speedup" }
+	| { strategy: "override"; overrideMillis: number }
+	| undefined
 > {
-  if (!defaultThrottleMillis || defaultThrottleMillis <= 0) {
-    throw new Error(
-      "defaultThrottleMillis is required and must be a positive number.",
-    );
-  }
-  if (!maxThrottleMillis || maxThrottleMillis <= 0) {
-    throw new Error(
-      "maxThrottleMillis is required and must be a positive number.",
-    );
-  }
+	if (!defaultThrottleMillis || defaultThrottleMillis <= 0) {
+		throw new Error(
+			"defaultThrottleMillis is required and must be a positive number.",
+		);
+	}
+	if (!maxThrottleMillis || maxThrottleMillis <= 0) {
+		throw new Error(
+			"maxThrottleMillis is required and must be a positive number.",
+		);
+	}
 
-  let throttleMillis = defaultThrottleMillis;
-  let lastYieldTimeMillis = 0;
+	let throttleMillis = defaultThrottleMillis;
+	let lastYieldTimeMillis = 0;
 
-  while (true) {
-    const currentTimeMillis = Date.now();
-    const millisSinceLastYield = currentTimeMillis - lastYieldTimeMillis;
+	while (true) {
+		const currentTimeMillis = Date.now();
+		const millisSinceLastYield = currentTimeMillis - lastYieldTimeMillis;
 
-    if (millisSinceLastYield >= throttleMillis) {
-      lastYieldTimeMillis = currentTimeMillis;
-    } else {
-      const remainingThrottleMillis = throttleMillis -
-        millisSinceLastYield;
-      console.log(
-        "Throttling for %s seconds...",
-        (remainingThrottleMillis / 1000).toFixed(2),
-      );
-      await new Promise((res) => setTimeout(res, remainingThrottleMillis));
-      lastYieldTimeMillis = Date.now();
-    }
-    const next = yield { latestThrottleMillis: throttleMillis };
-    switch (next?.strategy) {
-      case "backoff": {
-        const backoffThrottleMillis = throttleMillis * backoffMultiplier +
-          intBetween(1, 1000);
-        throttleMillis = Math.min(
-          maxThrottleMillis,
-          backoffThrottleMillis,
-        );
-        break;
-      }
+		if (millisSinceLastYield >= throttleMillis) {
+			lastYieldTimeMillis = currentTimeMillis;
+		} else {
+			const remainingThrottleMillis = throttleMillis -
+				millisSinceLastYield;
+			console.log(
+				"Throttling for %s seconds...",
+				(remainingThrottleMillis / 1000).toFixed(2),
+			);
+			await new Promise((res) => setTimeout(res, remainingThrottleMillis));
+			lastYieldTimeMillis = Date.now();
+		}
+		const next = yield { latestThrottleMillis: throttleMillis };
+		switch (next?.strategy) {
+			case "backoff": {
+				const backoffThrottleMillis = throttleMillis * backoffMultiplier +
+					intBetween(1, 1000);
+				throttleMillis = Math.min(
+					maxThrottleMillis,
+					backoffThrottleMillis,
+				);
+				break;
+			}
 
-      case "speedup": {
-        const speedupThrottleMillis = throttleMillis / backoffMultiplier +
-          intBetween(1, 1000);
-        throttleMillis = Math.max(
-          defaultThrottleMillis,
-          speedupThrottleMillis,
-        );
-        break;
-      }
+			case "speedup": {
+				const speedupThrottleMillis = throttleMillis / backoffMultiplier +
+					intBetween(1, 1000);
+				throttleMillis = Math.max(
+					defaultThrottleMillis,
+					speedupThrottleMillis,
+				);
+				break;
+			}
 
-      case "override":
-        throttleMillis = next.overrideMillis;
-        break;
+			case "override":
+				throttleMillis = next.overrideMillis;
+				break;
 
-      default:
-        throttleMillis = defaultThrottleMillis;
-        break;
-    }
+			default:
+				throttleMillis = defaultThrottleMillis;
+				break;
+		}
 
-    throttleMillis = Math.trunc(throttleMillis);
-  }
+		throttleMillis = Math.trunc(throttleMillis);
+	}
 }
 
 export { create as createThrottle };
 
 export interface CreateThrottleOptions {
-  defaultThrottleMillis: number;
-  maxThrottleMillis: number;
-  backoffMultiplier?: number;
+	defaultThrottleMillis: number;
+	maxThrottleMillis: number;
+	backoffMultiplier?: number;
 }
 
 export type ThrottleOptions =
-  | "backoff"
-  | "speedup"
-  | { strategy: "backoff" | "speedup" }
-  | { strategy: "override"; overrideMillis: number };
+	| "backoff"
+	| "speedup"
+	| { strategy: "backoff" | "speedup" }
+	| { strategy: "override"; overrideMillis: number };
