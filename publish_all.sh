0ad67549aeaef0d15b50cd08087db6fab28ec5bb
Warren Halderman <halderman.warren@gmail.com>
2025-07-24 14:14:04 +0900

    add publish script

---
+++ publish_all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+$(cd src/dated; deno publish --allow-dirty) &
+$(cd src/perf; deno publish --allow-dirty) &
+$(cd src/random; deno publish --allow-dirty) &
+$(cd src/regex; deno publish --allow-dirty) &
+$(cd src/throttle; deno publish --allow-dirty) &
+wait
