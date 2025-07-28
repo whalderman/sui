3fc70feb4c01bccd2a89e393b59e153c17089f18
Warren Halderman <halderman.warren@gmail.com>
2025-07-29 07:46:36 +0900

    workspaces cannot also act as packages/modules

--- mod.ts
+++
@@ -1,5 +0,0 @@
-export * as dated from "./src/dated/mod.ts";
-export * as perf from "./src/perf/mod.ts";
-export * as random from "./src/random/mod.ts";
-export * as regex from "./src/regex/mod.ts";
-export * as throttle from "./src/throttle/mod.ts";
---
+++ src/sui/mod.ts
@@ -0,0 +1,5 @@
+export * as dated from "@wjh/dated";
+export * as perf from "@wjh/perf";
+export * as random from "@wjh/random";
+export * as regex from "@wjh/regex";
+export * as throttle from "@wjh/throttle";
