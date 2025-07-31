afa9b1287f27528f1c58cdc67af700eb59ccc823
Warren Halderman <halderman.warren@gmail.com>
2025-07-31 16:42:10 +0900

    remove unnecessary Array<T> type

--- src/random/mod.ts
+++ src/random/mod.ts
@@ -10,6 +10,6 @@ export * as secure from "./secure/mod.ts";
 
 import * as integer from "./integer.ts";
 
-export function itemFrom<T>(array: ArrayLike<T> | Array<T>): T {
+export function itemFrom<T>(array: ArrayLike<T>): T {
 	return array[integer.between(0, array.length)];
 }
